7a6bcc62e022f659bde210c79efa70f4bbca262b
Koson Trachu <koson.trachu@gmail.com>
2016-08-25 06:31:27 +0700

    class1

---
+++ Week3_public_Modifier/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_public_Modifier
+{
+    class PointTest
+    {
+        public int x;
+        public int y;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PointTest p = new PointTest();
+            p.x = 10;
+            p.y = 15;
+            Console.WriteLine("x = {0}, y = {1}", p.x, p.y);
+        }
+    }
+}
